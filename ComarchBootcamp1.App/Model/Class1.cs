9793cf12964d3e3add1d7d193c60e9ac4194f7e2
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2024-09-15 11:27:04 +0200

    Initial commit

---
+++ ComarchBootcamp1.App/Model/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComarchBootcamp1.App.Model {
+    /// <summary>
+    /// Moja fajna klasa 1
+    /// </summary>
+    public class Auto {
+        protected string marka = "Audi";
+    }
+
+    public class Car : Auto {
+        public Car() {
+            marka = "BMW";
+        }
+    }
+
+    public class Truck : Car {
+        public Truck() {
+            marka = "Volvo";
+        }
+    }
+
+    public class Bus : Auto {
+        public Bus() { }
+    }
+}
---
+++ ComarchBootcamp1.App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using System.IO;
+using ComarchBootcamp1.App.Model;
+
+namespace ComarchBootcamp1.App {
+    public class Program {
+        protected short zmienna2 = 2;
+        //Jakiś komentarz
+
+        /*
+        Jakiś komentarz
+        Jakiś komentarz
+        Jakiś komentarz
+        Jakiś komentarz
+        */
+
+        /// <summary>
+        /// Metoda Main <see cref="decimal"/>
+        /// </summary>
+        /// <remarks>
+        /// <example>
+        /// <code></code>
+        /// </example>
+        /// </remarks>
+        /// <param name="args">Parametr args</param>
+        public static void Main(string[] args) {
+            Console.WriteLine("\u255d");
+        }
+    }
+}
