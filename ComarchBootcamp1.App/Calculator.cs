9e6c0e7a512d453132d0e5f48a4eae279612fdcf
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-09-28 12:08:01 +0200

    Program Kalkulator

---
+++ ComarchBootcamp1.App/Calculator.cs
@@ -0,0 +1,40 @@
+﻿namespace ComarchBootcamp1.App
+{
+    internal class Calculator
+    {
+        public int Add(params int[] values)
+        {
+            int result = 0;
+            foreach (int i in values)
+            {
+                result += i;
+            }
+            return result;
+        }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        internal int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        internal int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        internal float Dividy(int x, int y)
+        {
+            return (float)x / y;
+        }
+
+        internal float Modulo(int x, int y)
+        {
+            return (float)x % y;
+        }
+    }
+}
--- ComarchBootcamp1.App/Program.cs
+++ ComarchBootcamp1.App/Program.cs
@@ -1,79 +1,77 @@
-﻿using System.Text.Json;
-using System.IO;
-using ComarchBootcamp1.App.Model;
+﻿namespace ComarchBootcamp1.App;
 
-namespace ComarchBootcamp1.App
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        protected short zmienna2 = 2;
-        //Jakiś komentarz
-
-        /*
-        Jakiś komentarz
-        Jakiś komentarz
-        Jakiś komentarz
-        Jakiś komentarz
-        */
-
-        /// <summary>
-        /// Metoda Main <see cref="decimal"/>
-        /// </summary>
-        /// <remarks>
-        /// <example>
-        /// <code></code>
-        /// </example>
-        /// </remarks>
-        /// <param name="args">Parametr args</param>
-        public static void Main(string[] args)
+        ConsoleKeyInfo yesNo;
+        do
         {
-            //Console.WriteLine("Witaj, jak masz na imię?");
-            //string imie = Console.ReadLine();
-            //Console.Write("Podaj wiek: ");
-            //string sWiek = Console.ReadLine();
-            //int wiek = Convert.ToInt32(sWiek);
-            //int wiek2 = int.Parse(sWiek);
+            Console.Clear();
+            Console.WriteLine("KALCULATOR 1.0)");
+            Console.WriteLine("1. Dodawanie");
+            Console.WriteLine("2. Odejmowanie");
+            Console.WriteLine("3. Mnożenie");
+            Console.WriteLine("4. Dzielenie");
+            Console.WriteLine("5. Reszta z dzielenia");
+            Console.WriteLine("0. Wyjście z programu");
+            Console.Write("Wybierz opcję: ");
 
-            //Console.WriteLine("Witaj " + imie + ", masz " + wiek2 + " lat");
-            //Console.WriteLine(string.Concat("Witaj ", imie, ", masz ", wiek, " lat"));
-            //Console.WriteLine("Witaj {0}, masz {1} lat", imie, wiek2);
-            //Console.WriteLine($"Witaj {imie}, masz {wiek} lat");
-            //int rok = DateTime.Now.Year - wiek;
-
-            //Console.WriteLine($"Urodziłeś się w {rok} roku");
-
-            Console.WriteLine("Sortowanie bąbęlkowe");
-            Console.WriteLine("Podaj elementy tablicy oddzielone przerwą: ");
-            string line = Console.ReadLine();
-            string[] elements = line.Split([' ', ',', ';'], StringSplitOptions.RemoveEmptyEntries);
-            int[] tab = new int[elements.Length];
-
-            // konwersja tablicy stringów na tablicę intów
-            for (int i = 0; i < elements.Length; i++)
+            if (int.TryParse(Console.ReadLine(), out int choise))
             {
-                tab[i] = int.Parse(elements[i]);
-            }
+                if (choise == 0) Environment.Exit(0);
 
-            // sortowanie bąbelkowe
-            for (int i = 0; i < tab.Length - 1; i++)
-            {
-                for (int j = 0; j < tab.Length - 1; j++)
+                Console.Clear();
+                Console.Write("Podaj X: ");
+                int x = int.Parse(Console.ReadLine());
+
+                Console.Write("Podaj Y: ");
+                int y = int.Parse(Console.ReadLine()!);
+
+                Calculator calc = new Calculator();
+                float result = 0;
+
+                switch (choise)
                 {
-                    if (tab[j] > tab[j + 1])
-                    {
-                        int tmp = tab[j];
-                        tab[j] = tab[j + 1];
-                        tab[j + 1] = tmp;
-                    }
+                    case 1:
+                        result = calc.Add(x, y);
+                        Console.WriteLine($"Wynik dodawania {x} oraz {y} to {result:0}");
+                        break;
+                    case 2:
+                        result = calc.Subtract(x, y);
+                        Console.WriteLine($"Wynik odejmowania {x} oraz {y} to {result:0}");
+                        break;
+                    case 3:
+                        result = calc.Multiply(x, y);
+                        Console.WriteLine($"Wynik mnożenia {x} oraz {y} to {result:0}");
+                        break;
+                    case 4:
+                        result = calc.Dividy(x, y);
+                        Console.WriteLine($"Wynik dzielenia {x} oraz {y} to {result:f}");
+                        break;
+                    case 5:
+                        result = calc.Modulo(x, y);
+                        Console.WriteLine($"Reszta z dzielenia {x} oraz {y} to {result:f}");
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wprowadzona wartość jest poza zakresem.");
+                        Console.ResetColor();
+                        break;
                 }
             }
-
-            Console.WriteLine("Posortowana tablica: ");
-            foreach (int item in tab)
+            else
             {
-                Console.Write(item + " ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Wprowadzona wartość jest nieprawidłowa.");
+                Console.ResetColor();
             }
+            Console.ReadKey();
+            Console.Clear();
+            Console.Write("Czy chcesz wykonać kolejną operację? [T|n]");
+            yesNo = Console.ReadKey();
 
-        }
+        } while (yesNo.Key != ConsoleKey.N);
+
     }
 }
